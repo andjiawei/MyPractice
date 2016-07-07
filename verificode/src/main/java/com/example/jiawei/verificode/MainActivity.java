7c3a1928bfa805c1843fe7e486321144af6d329f
jiawei <361556977@qq.com>
2016-07-07 17:21:06 +0800

    增加点击画线textView跳转到webView

--- verificode/src/main/java/com/example/jiawei/verificode/MainActivity.java
+++ verificode/src/main/java/com/example/jiawei/verificode/MainActivity.java
@@ -1,12 +1,15 @@
 package com.example.jiawei.verificode;
 
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Html;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +23,14 @@ public class MainActivity extends AppCompatActivity {
 
         TextView textView= (TextView) findViewById(R.id.textView);
         textView.setText(instance.getCode());
+        textView.append(Html.fromHtml("<u>"+"0123456"+"</u>"));
+        textView.setOnClickListener(this);
+
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        startActivity(new Intent(this,WebViewActivity.class));
     }
 }
---
+++ verificode/src/main/java/com/example/jiawei/verificode/WebViewActivity.java
@@ -0,0 +1,18 @@
+package com.example.jiawei.verificode;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebView;
+
+public class WebViewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_web_view);
+
+        WebView webView = (WebView) findViewById(R.id.webView);
+
+        webView.loadUrl("file:///android_asset/bbb.html");
+    }
+}
